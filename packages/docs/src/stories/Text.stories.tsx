d4561aef9bd15e95a0fa1b459843011ab6db8bce
Bernardo Batistelli <bebatistelli254@gmail.com>
2023-10-31 14:39:45 -0300

    adding components to storybook

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium inventore fuga quia! Repellendus minus expedita esse. Veniam placeat eveniet nostrum tenetur sapiente, sunt minus eligendi. Pariatur dolorem tenetur quibusdam est!',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
